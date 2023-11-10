b0994a835bdf05c4e215d65eae41bf618f40d977
Jinjae Park <parkjinjae0627@gmail.com>
2023-11-10 14:21:03 +0900

    update

---
+++ Ch5/028_1167.py
@@ -0,0 +1,40 @@
+# 028
+# boj 1167
+
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+
+def bfs(g, s):
+    visited = [False] * (n + 1)
+    visited[s] = True
+    q = deque([s])
+    max_dist = [0] * (n + 1)
+
+    while q:
+        v = q.popleft()
+        for i in g[v]:
+            if not visited[i[0]]:
+                visited[i[0]] = True
+                q.append(i[0])
+                max_dist[i[0]] = max_dist[v] + i[1]
+
+    return max_dist
+
+
+n = int(input())
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(n):
+    a = list(map(int, input().split()))
+    for i in range(1, len(a) - 1, 2):
+        graph[a[0]].append((a[i], a[i + 1]))
+
+max_dist = bfs(graph, 1)
+
+max_idx = max_dist.index(max(max_dist))
+max_dist = bfs(graph, max_idx)
+
+print(max(max_dist))
