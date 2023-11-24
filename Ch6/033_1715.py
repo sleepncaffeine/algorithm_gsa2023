1525fa8541c0b72edeb4905b3414271bb9f63db2
Jinjae Park <parkjinjae0627@gmail.com>
2023-11-24 14:46:22 +0900

    update

---
+++ Ch6/033_1715.py
@@ -0,0 +1,19 @@
+# 033
+# boj 1715
+
+from heapq import heappush, heappop
+
+n = int(input())
+heap = []
+
+for _ in range(n):
+    heappush(heap, int(input()))
+
+ans = 0
+while len(heap) > 1:
+    a = heappop(heap)
+    b = heappop(heap)
+    ans += a + b
+    heappush(heap, a + b)
+
+print(ans)
---
+++ Ch6/036_1541.py
@@ -0,0 +1,31 @@
+# 036
+# boj 1541
+
+
+s = input()
+
+numbers = []
+operators = []
+
+num = ""
+for c in s:
+    if c.isdigit():
+        num += c
+    else:
+        numbers.append(int(num))
+        operators.append(c)
+        num = ""
+numbers.append(int(num))
+
+ans = numbers[0]
+
+flag = False
+for i in range(len(operators)):
+    if operators[i] == "-":
+        flag = True
+    if flag:
+        ans -= numbers[i + 1]
+    else:
+        ans += numbers[i + 1]
+
+print(ans)
